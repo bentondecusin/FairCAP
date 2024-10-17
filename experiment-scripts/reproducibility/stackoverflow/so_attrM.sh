8637c8b599df13e3cc93cd7bd5a7dedd4f9addd9
Benton Li <cl2597@cornell.edu>
2024-10-17 17:39:34 +0100

    update scripts

--- experiment-scripts/reproducibility/stackoverflow/so_attrM.sh
+++ experiment-scripts/reproducibility/stackoverflow/so_attrM.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 pkill -f ssh
 python ../run_experiment.py stackoverflow/config_lo_attrM.json so/remote_lo_attrM.json &
-python ../run_experiment.py stackoverflow/config_md_attrM.json so/remote_md_attrM.json &
+python ../run_experiment.py stackoverflow/config_md_attrM.json so/remote_md_attrM.json 
 
 
 
