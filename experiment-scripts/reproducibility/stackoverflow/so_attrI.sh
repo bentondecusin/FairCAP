bb753bedc0ff54426ee380942c555b5acff4d0dc
Benton Li <cl2597@cornell.edu>
2024-10-17 17:23:16 +0100

    Load massive configs like batshit

---
+++ experiment-scripts/reproducibility/stackoverflow/so_attrI.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+pkill -f ssh
+python ../run_experiment.py stackoverflow/config_lo_attrI.json so/remote_lo_attrI.json  &
+python ../run_experiment.py stackoverflow/config_md_attrI.json so/remote_md_attrI.json &
+
+
+
+
+
