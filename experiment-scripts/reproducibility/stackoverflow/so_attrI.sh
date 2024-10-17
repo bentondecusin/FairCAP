234ee051f75b835c95cd7f0f67e8f246ad435b5c
Benton Li <cl2597@cornell.edu>
2024-10-17 19:53:39 +0100

    stage 5

--- experiment-scripts/reproducibility/stackoverflow/so_attrI.sh
+++ experiment-scripts/reproducibility/stackoverflow/so_attrI.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 pkill -f ssh
-python ../run_experiment.py stackoverflow/config_lo_attrI.json so/remote_lo_attrI.json  &
-python ../run_experiment.py stackoverflow/config_md_attrI.json so/remote_md_attrI.json 
+python ../run_experiment.py stackoverflow/config_lo_attrI.json so/remote_lo_attrI.json 
 
 
 
