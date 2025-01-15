32cecafee0fd3b226236487308048555706be9bf
Benton Li <cl2597@cornell.edu>
2025-01-15 22:14:37 +0000

    add robustness scripts

---
+++ experiment-scripts/reproducibility/stackoverflow/so_default.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+pkill -f ssh
+python ../run_experiment.py stackoverflow/default_dag/default_full.json so/remote_full.json
+
+
+
+
---
+++ experiment-scripts/reproducibility/stackoverflow/so_pc_chisq.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+pkill -f ssh
+python ../run_experiment.py stackoverflow/pc_chisq/pc_chisq_full.json so/remote_full.json
+
+
+
+
